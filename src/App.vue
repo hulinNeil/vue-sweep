4deba9174239523d93d98e20b222d35c7b0cf958
hulinNeil <1289739946@qq.com>
2019-09-30 16:13:12 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,63 @@
 <template>
-	<div id="app">
-		<div v-if="!isShowSeep" class="input">
-			<input type="number" @input="bindInputCol" placeholder="列数">
-			<input type="number" @input="bindInputRow" placeholder="行数">
-			<input type="number" @input="bindInputNum" placeholder="雷数">
-			<button @click="bindClick">确定</button>
-		</div>
-		<Sweep :maxRow="row" :maxCol="col" :sweepNum="num" v-else />
-	</div>
+  <div id="app">
+    <div v-if="!isShowSeep" class="input">
+      <input type="number" @input="bindInputCol" placeholder="列数" />
+      <input type="number" @input="bindInputRow" placeholder="行数" />
+      <input type="number" @input="bindInputNum" placeholder="雷数" />
+      <button @click="bindClick">确定</button>
+    </div>
+    <Sweep :maxRow="row" :maxCol="col" :sweepNum="num" v-else />
+  </div>
 </template>
 
 <script>
-	import Sweep from './components/Sweep.vue'
+import Sweep from "./components/Sweep.vue";
 
-	export default {
-		components: {
-			Sweep
-		},
-		data() {
-			return {
-				row: 0,
-				col: 0,
-				num: 0,
-				isShowSeep: false
-			}
-		},
-		created(){
-			console.log(1234);
-			console.log(2);
-		},
-		methods: {
-			bindInputCol(e){
-				console.log('我这里是dev分支');
-				this.col = Number(e.target.value);
-			},
-			bindInputRow(e){
-				this.row = Number(e.target.value);
-			},
-			bindInputNum(e){
-				this.num = Number(e.target.value);
-			},
-			bindClick() {
-				if (this.col * this.row * this.num !== 0) {
-					this.isShowSeep = true;
-				}else{
-					alert('请输入正确值~。。。78阿斯顿~')
-				}
-			}
-		}
-	}
+export default {
+  components: {
+    Sweep
+  },
+  data() {
+    return {
+      row: 0,
+      col: 0,
+      num: 0,
+      isShowSeep: false
+    };
+  },
+  created() {
+    console.log(1234);
+    console.log(2);
+  },
+  methods: {
+    bindInputCol(e) {
+      console.log("我这里是dev分支");
+      this.col = Number(e.target.value);
+    },
+    bindInputRow(e) {
+      this.row = Number(e.target.value);
+    },
+    bindInputNum(e) {
+      this.num = Number(e.target.value);
+    },
+    bindClick() {
+      if (this.col * this.row * this.num !== 0) {
+        this.isShowSeep = true;
+      } else {
+        alert("请输入正确值~。。。78阿斯顿~");
+      }
+    }
+  }
+};
 </script>
 
 <style>
-	.input {
-		display: flex;
-		flex-direction: column;
-		height: 100px;
-		width: 200px;
-		margin: 0 auto;
-		justify-content: space-around;
-	}
+.input {
+  display: flex;
+  flex-direction: column;
+  height: 100px;
+  width: 200px;
+  margin: 0 auto;
+  justify-content: space-around;
+}
 </style>
